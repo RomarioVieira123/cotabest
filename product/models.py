69d457192ef43b17f41ac176880a320407e51853
romar <romario.lara.vieira@gmail.com>
2022-11-14 20:39:42 -0300

    Update project

--- product/models.py
+++ product/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    price = models.FloatField(default=0.0)
+    minimun = models.PositiveIntegerField(default=0)
+    amount_per_package = models.PositiveIntegerField(default=0)
+    max_availability = models.PositiveIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(default=timezone.now)
